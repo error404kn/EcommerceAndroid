e426b12c703c4219624fe308b827ecff30a9c43c
error404kn <scandaluzumaki@gmail.com>
2024-01-15 19:15:31 +0400

    Add Cart Unfinished

--- app/src/main/java/com/example/itstepproject/fragments/shopping/ProductDetailsFragment.kt
+++ app/src/main/java/com/example/itstepproject/fragments/shopping/ProductDetailsFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,15 +15,26 @@ import com.example.itstepproject.R
 import com.example.itstepproject.activities.ShoppingActivity
 import com.example.itstepproject.adapters.ColorsAdapter
 import com.example.itstepproject.adapters.ViewPager2Images
+import com.example.itstepproject.data.CartProduct
 import com.example.itstepproject.databinding.FragmentProductDetailsBinding
+import com.example.itstepproject.util.Resource
 import com.example.itstepproject.util.hideBottomNavigationView
+import com.example.itstepproject.viewmodel.DetailsViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class ProductDetailsFragment: Fragment() {
     private val args by navArgs<ProductDetailsFragmentArgs>()
     private lateinit var binding: FragmentProductDetailsBinding
     private val viewPagerAdapter by lazy { ViewPager2Images() }
     private val colorsAdapter by lazy { ColorsAdapter() }
+
+    //Cart
+    private var selectedColor: Int? = null
+    private val viewModel by viewModels<DetailsViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -43,6 +57,36 @@ class ProductDetailsFragment: Fragment() {
         binding.imgClose.setOnClickListener{
             findNavController().navigateUp()
         }
+
+        //Cart
+        colorsAdapter.onItemClick = {
+            selectedColor = it
+        }
+
+        binding.buttonAddToCart.setOnClickListener{
+            viewModel.addUpdateProductInCart(CartProduct(product, 1, selectedColor))
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch{
+            viewModel.addToCart.collectLatest {
+                when(it){
+                    is Resource.Loading -> {
+                        binding.buttonAddToCart.startAnimation()
+                    }
+                    is Resource.Success -> {
+                        binding.buttonAddToCart.revertAnimation()
+                        binding.buttonAddToCart.setBackgroundColor(resources.getColor(R.color.green))
+                        Toast.makeText(requireContext(),"პროდუქტი წარმატებით დაემატა", Toast.LENGTH_SHORT).show()
+                    }
+                    is Resource.Error -> {
+                        binding.buttonAddToCart.stopAnimation()
+                        Toast.makeText(requireContext(),it.message, Toast.LENGTH_SHORT).show()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+
 
         binding.apply {
             tvProductName.text = product.name
