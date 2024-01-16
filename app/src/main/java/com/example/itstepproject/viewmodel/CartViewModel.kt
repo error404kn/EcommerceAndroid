55a4079cc0834e67912b5dc6bfd00255f2fa97c2
error404kn <scandaluzumaki@gmail.com>
2024-01-16 21:06:31 +0400

    Cart Finished

--- app/src/main/java/com/example/itstepproject/viewmodel/CartViewModel.kt
+++ app/src/main/java/com/example/itstepproject/viewmodel/CartViewModel.kt
@@ -4,14 +4,18 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.itstepproject.data.CartProduct
 import com.example.itstepproject.firebase.FirebaseCommon
+import com.example.itstepproject.helper.getProductPrice
 import com.example.itstepproject.util.Resource
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.toObjects
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -27,7 +31,37 @@ class CartViewModel @Inject constructor(
         MutableStateFlow<Resource<List<CartProduct>>>(Resource.Unspecified())
     val cartProducts = _cartProducts.asStateFlow()
 
+    val productsPrice = cartProducts.map {
+        when(it){
+            is Resource.Success -> {
+                calculatePrice(it.data)
+            }else -> null
+        }
+    }
+    private val _deleteDialog = MutableSharedFlow<CartProduct>()
+    val deleteDialog = _deleteDialog.asSharedFlow()
+
     private var cartProductDocuments = emptyList<DocumentSnapshot>()
+
+
+    fun deleteCartProduct(cartProduct: CartProduct){
+        val index = cartProducts.value.data?.indexOf(cartProduct)
+        if (index != null && index != -1){
+            val documentId = cartProductDocuments[index].id
+            firestore.collection("user").document(auth.uid!!).collection("cart")
+                .document(documentId).delete()
+        }
+    }
+
+
+
+    private fun calculatePrice(data: List<CartProduct>?): Float {
+        return data?.let { nonNullData ->
+            nonNullData.sumByDouble { cartProduct ->
+                (cartProduct.product.offerPercentage.getProductPrice(cartProduct.product.price) * cartProduct.quantity).toDouble()
+            }.toFloat()
+        } ?: 0f
+    }
 
     init {
         getCartProducts()
@@ -53,15 +87,22 @@ class CartViewModel @Inject constructor(
         cartProduct: CartProduct,
         quantityChanging: FirebaseCommon.QuantityChanging
     ) {
+
         val index = cartProducts.value.data?.indexOf(cartProduct)
 
         if (index != null && index != -1){
             val documentId = cartProductDocuments[index].id
             when(quantityChanging){
                 FirebaseCommon.QuantityChanging.INCREASE -> {
+                    viewModelScope.launch { _cartProducts.emit(Resource.Loading()) }
                     increaseQuantity(documentId)
                 }
                 FirebaseCommon.QuantityChanging.DECREASE ->{
+                    if (cartProduct.quantity == 1){
+                        viewModelScope.launch{_deleteDialog.emit(cartProduct)}
+                        return
+                    }
+                    viewModelScope.launch { _cartProducts.emit(Resource.Loading()) }
                     decreaseQuantity(documentId)
                 }
             }
