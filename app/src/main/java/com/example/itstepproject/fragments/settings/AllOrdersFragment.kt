0ef0ba74a8004fa6c8273250e6346764ac94bf92
error404kn <scandaluzumaki@gmail.com>
2024-01-17 23:48:02 +0400

    Order Detail

--- app/src/main/java/com/example/itstepproject/fragments/settings/AllOrdersFragment.kt
+++ app/src/main/java/com/example/itstepproject/fragments/settings/AllOrdersFragment.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.itstepproject.adapters.AllOrdersAdapter
@@ -58,6 +59,11 @@ class AllOrdersFragment: Fragment() {
                     else -> Unit
                 }
             }
+        }
+
+        ordersAdapter.onClick = {
+            val action = AllOrdersFragmentDirections.actionOrderFragmentToOrderDetailFragment(it)
+            findNavController().navigate(action)
         }
     }
 
---
+++ app/src/main/java/com/example/itstepproject/fragments/settings/OrderDetailFragment.kt
@@ -0,0 +1,75 @@
+package com.example.itstepproject.fragments.settings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.itstepproject.adapters.BillingProductsAdapter
+import com.example.itstepproject.data.order.OrderStatus
+import com.example.itstepproject.data.order.getOrderStatus
+import com.example.itstepproject.databinding.FragmentOrderDetailBinding
+import com.example.itstepproject.util.VerticalItemDecoration
+
+class OrderDetailFragment: Fragment() {
+    private lateinit var binding: FragmentOrderDetailBinding
+    private val billingProductsAdapter by lazy { BillingProductsAdapter() }
+    private val args by navArgs<OrderDetailFragmentArgs>()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentOrderDetailBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val order = args.order
+        setupOrderRv()
+
+        binding.apply {
+            tvOrderId.text = "Order #${order.orderId}"
+            stepView.setSteps(
+                mutableListOf(
+                    OrderStatus.Ordered.status,
+                    OrderStatus.Confirmed.status,
+                    OrderStatus.Shipped.status,
+                    OrderStatus.Delivered.status,
+                )
+            )
+            val currentOrderState = when(getOrderStatus(order.orderStatus)){
+                is OrderStatus.Ordered -> 0
+                is OrderStatus.Confirmed -> 1
+                is OrderStatus.Shipped -> 2
+                is OrderStatus.Delivered -> 3
+                else -> 0
+            }
+            stepView.go(currentOrderState, false)
+            if (currentOrderState == 3){
+                stepView.done(true)
+            }
+            tvFullName.text = order.address.fullName
+            tvAddress.text = "${order.address.street} ${order.address.city}"
+            tvPhoneNumber.text = order.address.phone
+
+            tvTotalPrice.text = "$ ${order.totalPrice}"
+        }
+
+        billingProductsAdapter.differ.submitList(order.products)
+    }
+
+    private fun setupOrderRv() {
+        binding.rvProducts.apply {
+            adapter = billingProductsAdapter
+            layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+            addItemDecoration(VerticalItemDecoration())
+        }
+    }
+}
